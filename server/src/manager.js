a1043e077f088d5abcce1f6b29dd8c42be778207
Alec Bernardi <bernaa2@rpi.edu>
2018-02-14 10:59:25 -0500

    Add Manager class

---
+++ server/src/manager.js
@@ -0,0 +1,26 @@
+const Game = require('./game')
+
+class Manager {
+  constructor() {
+    this.games = {};
+  }
+
+  makeGame(id, seed) {
+    this.games[id] = new Game(id, seed);
+  }
+
+  getGame(id) {
+    return this.games[id];
+  }
+
+  removeGame(id) {
+    game = this.games[id];
+    if (game) {
+      delete this.games[id];
+      return game.state;
+    } else return undefined;
+  }
+
+}
+
+module.exports = Manager;
