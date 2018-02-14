a77e2862817c1618579656b0af238ce45ba7f106
Alec Bernardi <bernaa2@rpi.edu>
2018-02-14 11:04:01 -0500

    Add simple game management

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 
-let games = {};
+const Manager = require('./manager');
 
-app.get('*', (req, res) => res.send('Hello, World!'));
+let manager = new Manager();
+
+app.route('/game/:gameId/')
+  // Starting a game
+  .post(function (req, res) {
+    gameId = req.params.gameId;
+    if (!manager.getGame(gameId)) {
+      manager.makeGame(gameId, req.query.seed);
+      res.sendStatus(201);
+    } else res.sendStatus(409);
+  })
+  // Observing a game
+  .get(function (req, res) {
+    gameId = req.params.gameId;
+    game = manager.getGame(gameId);
+    if (game) {
+      res.status(201).send(game.state);
+    } else res.sendStatus(404);
+  })
+  // Removing a game
+  .delete(function (req, res) {
+    // TODO: Check if game is in progress
+    game = manager.removeGame(req.params.gameId);
+    if (game)
+      res.status(200).send(game.state);
+    else res.sendStatus(404);
+  });
 
 app.listen(4040, () => console.log('Listening on port 4040!'));
