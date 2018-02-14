9b944f56047ec7e13ec834bb0ca079d44cf64ba5
Alec Bernardi <bernaa2@rpi.edu>
2018-02-13 22:52:42 -0500

    Change to expressjs Hello World

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+const express = require('express');
+const app = express();
+
+let games = {};
+
+app.get('*', (req, res) => res.send('Hello, World!'));
+
+app.listen(4040, () => console.log('Listening on port 4040!'));
